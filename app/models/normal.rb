34768968424e2bba7f84f7fb6a886de16626a844
Lucius Choi <lucius.choi@gmail.com>
2016-12-10 20:21:11 +0900

    fixed norma.rb class file

--- app/models/normal.rb
+++ app/models/normal.rb
@@ -3,19 +3,25 @@ require 'open-uri'
 
 class Normal < ActiveRecord::Base
     validates :validation, :uniqueness => true
-    normaltable = Nokogiri::HTML(open(File.open("교양테이블.html")))
-    
-    @normal = normaltable.css("table#culLctTmtblDscTbl tbody tr")
-    @normal.each do |t|
-    
-    Normal.create(
-        div: t.css("td")[1].text,
-        title: t.css("td.ta_l a").text.remove("(타학년 제한없음 2차때 수강가능)").remove("(타학년 제한없음, 2차때 신청가능)"),
-        grades: t.css("td")[5].text,
-        prof: t.css("td")[6].text,
-        time: t.css("td")[7].text.split("(")[0],
-        classroom: t.css("td")[7].text.remove(")").split("(")[1],
-        validation: t.css("td")[0].text
-    )
+    if Normal.count.zero?
+      normaltable = Nokogiri::HTML(open(File.open("교양테이블.html")))
+
+      @normal = normaltable.css("table#culLctTmtblDscTbl tbody tr")
+      @normal.each do |t|
+        Normal.create(
+            div: t.css("td")[1].text,
+            title: t.css("td.ta_l a").text.remove("(타학년 제한없음 2차때 수강가능)").remove("(타학년 제한없음, 2차때 신청가능)"),
+            grades: t.css("td")[5].text,
+            prof: t.css("td")[6].text,
+            time: t.css("td")[7].text.split("(")[0],
+            classroom: t.css("td")[7].text.remove(")").split("(")[1],
+            validation: t.css("td")[0].text
+        )
+      end
     end
+
+    def self.search( q )
+      Normal.where("title like ?", "%#{q}%")
+    end
+
 end
