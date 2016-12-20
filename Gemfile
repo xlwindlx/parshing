5bad5c934dc284fcd285812d15b68e38bdb798b8
854575 <854575@naver.com>
2016-12-20 07:49:52 +0000

    col 수정완료

--- Gemfile
+++ Gemfile
@@ -2,8 +2,6 @@ source 'https://rubygems.org'
 
 gem 'bootstrap-sass'
 gem 'simple_form'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3', :group => :development
 gem 'rails_12factor', :group => :production
 gem 'pg', :group => :production
 gem 'whenever'
@@ -19,7 +17,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 gem 'therubyracer', platforms: :ruby
-
+# Use sqlite3 as the database for Active Record
+gem 'sqlite3', :group => :development
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,25 @@
 if Normal.count.zero?
   normaltable = Nokogiri::HTML(open(File.open("교양테이블.html")))
   
-  @normal = normaltable.css("table#culLctTmtblDscTbl tbody tr")
-  @normal.each do |t|
-    normal = Normal.create(
+  @normals = normaltable.css("table#culLctTmtblDscTbl tbody tr")
+  @normals.each do |t|
+    weekdays =  t.css("td")[7].text.split("/")
+    time, classroom = [], []
+    
+    weekdays.each do | weekday |
+      time << weekday.scan(/(.*)\(.*\)/).flatten.join(", ")
+      classroom_tmp = weekday.scan(/.*\((.*)\)/)
+      classroom << (classroom_tmp.nil? ? "" : classroom_tmp.join(", "))
+    end
+    time = time.join("\n")
+    classroom = classroom.join("\n")    
+    Normal.create(
       div: t.css("td")[1].text,
       title: t.css("td.ta_l a").text.remove("(타학년 제한없음 2차때 수강가능)").remove("(타학년 제한없음, 2차때 신청가능)"),
       grades: t.css("td")[5].text,
       prof: t.css("td")[6].text,
-      time: t.css("td")[7].text.split("(")[0].gsub("/","\n"),
-      classroom: t.css("td")[7].text.scan(/.*\((.*)\)/).last,
+      time: time,
+      classroom: classroom,
       validation: t.css("td")[0].text
     )
   end
@@ -21,18 +31,25 @@ end
 if Prof.count.zero?
     proftable = Nokogiri::HTML(open(File.open("전공테이블.html")))
     
-    @prof = proftable.css("table#mjLctTmtblDscTbl tbody tr")
-    @prof.each do |f|
-      timeclassroom = f.css("td")[9].text.scan(/(.*)\((.*)\)/)
-      time = timeclassroom.last[0].gsub("/","\n")
-      classroom = timeclassroom.last[1]
+    @profs = proftable.css("table#mjLctTmtblDscTbl tbody tr")
+    @profs.each do |f|
+      weekdays =  f.css("td")[9].text.split("/")
+      time, classroom = [], []
+      
+      weekdays.each do | weekday |
+        time << weekday.scan(/(.*)\(.*\)/).flatten.join(", ")
+        classroom_tmp = weekday.scan(/.*\((.*)\)/)
+        classroom << (classroom_tmp.nil? ? "" : classroom_tmp.join(", "))
+      end
+      time = time.join("\n")
+      classroom = classroom.join("\n")
       Prof.create(
           div: f.css("td")[3].text,
           title: f.css("td.ta_l a").text,
           grades: f.css("td")[7].text,
           proffesion: f.css("td")[8].text,
           time: time, 
-          classroom: (classroom.nil? ? "" : classroom.join("")), 
+          classroom: classroom, 
           grade: f.css("td")[2].text,
           subject: f.css("td")[1].text,
           validation: f.css("td")[0].text
