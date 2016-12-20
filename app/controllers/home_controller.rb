84ee9e411f25e2ef91c44b00fdecd4999d0c44b9
854575 <854575@naver.com>
2016-12-20 08:31:59 +0000

    col

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,48 +4,63 @@ class HomeController < ApplicationController
   end
   
   def find_normal
-    div = params[:div]
-    @divs = []
-    if div.present?
-      @divs = Normal.search('div', div)
-    end
     
-    title = params[:title]
-    @titles = []
-    if title.present?
-      @titles = Normal.search('title', title)
-    end
+    @normals = Normal.all
+    @normals = @normals.search('div', params[:div]) if params[:div]
+    @normals = @normals.search('title', params[:title]) if params[:title]
+    @normals = @normals.search('grades', params[:grades]) if params[:grades]
+    @normals = @normals.search('prof', params[:prof]) if params[:prof]
+    @normals = @normals.search('time', params[:time]) if params[:time]
+    @normals = @normals.search('classroom', params[:classroom]) if params[:classroom]
     
-    grades = params[:grades]
-    @Grades = []
-    if grades.present?
-      @Grades = Normal.search('grades',grades)
-    end
+    # div = params[:div]
+    # @divs = []
+    # if div.present?
+    #   @divs = Normal.search('div', div)
+    # end
     
-    prof = params[:prof]
-    @profs = []
-    if prof.present?
-      @profs = Normal.search('prof', prof)
-    end
+    # title = params[:title]
+    # @titles = []
+    # if title.present?
+    #   @titles = Normal.search('title', title)
+    # end
     
-    time = params[:time]
-    @times = []
-    if time.present?
-      @times = Normal.search('time', time)
-    end
+    # grades = params[:grades]
+    # @Grades = []
+    # if grades.present?
+    #   @Grades = Normal.search('grades',grades)
+    # end
     
-    classroom = params[:classroom]
-    @classrooms = []
-    if classroom.present?
-      @classrooms = Normal.search('classroom', classroom)
-    end
+    # prof = params[:prof]
+    # @profs = []
+    # if prof.present?
+    #   @profs = Normal.search('prof', prof)
+    # end
+    
+    # time = params[:time]
+    # @times = []
+    # if time.present?
+    #   @times = Normal.search('time', time)
+    # end
+    
+    # classroom = params[:classroom]
+    # @classrooms = []
+    # if classroom.present?
+    #   @classrooms = Normal.search('classroom', classroom)
+    # end
   end
   
   def find_prof
-    div = params[:div]
-    @divs = []
-    if div.present?
-      @divs = Prof.search(div)
-    end
+    
+    @profs = Prof.all
+    @profs = @profs.search('div', params[:div]) if params[:div]
+    @profs = @profs.search('title', params[:title]) if params[:title]
+    @profs = @profs.search('grades', params[:grades]) if params[:grades]
+    @profs = @profs.search('proffesion', params[:proffesion]) if params[:proffesion]
+    @profs = @profs.search('time', params[:time]) if params[:time]
+    @profs = @profs.search('classroom', params[:classroom]) if params[:classroom]
+    @profs = @profs.search('grade', params[:grade]) if params[:grade]
+    @profs = @profs.search('subject', params[:subject]) if params[:subject]
+    
   end
 end
